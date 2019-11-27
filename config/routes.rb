eb1711186cb2ff7475e825045be64a2a076c6d8f
Gwepura <aketani32@gmail.com>
2019-11-27 13:37:15 +0000

    Fix create, add flash message and error messages, create show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-<<<<<<< HEAD
-  
+
   resources :articles
-=======
->>>>>>> 40ac378e2b3f9e018a95134d5ab5f15ceb989c16
 end
